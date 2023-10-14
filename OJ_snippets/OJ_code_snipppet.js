fa17fe5496e3ef5e9b2c26cc9a06e243bdcd708d
Vishal Rathod <vrathod591@gmail.com>
2023-10-15 01:12:35 +0530

    code snippet for single input

---
+++ OJ_snippets/OJ_code_snipppet.js
@@ -0,0 +1,20 @@
+function runProgram(input){
+      var newInput = input.split("\n");
+      var data = newInput[1].trim().split(" ").map(Number);
+ 	  console.log(data);
+}
+process.stdin.resume();
+process.stdin.setEncoding("ascii");
+let read = "";
+process.stdin.on("data", function (input) {
+    read += input;
+});
+process.stdin.on("end", function () {
+    read = read.replace(/\n$/,"")
+   runProgram(read);
+});
+process.on("SIGINT", function () {
+    read = read.replace(/\n$/,"")
+    runProgram(read);
+    process.exit(0);
+});
